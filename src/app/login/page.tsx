868c9c375cda909a1692a5018eb18ebaabb8264d
Aaron Cedillo <aaroncescalante@gmail.com>
2025-03-07 20:20:06 -0500

    Diseño en login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState } from 'react';
 import { loginUser } from '../services/authService';
 
@@ -11,26 +11,22 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      // Llamamos a la función loginUser que hemos creado
       const response = await loginUser(email, password);
-
-      // Si el login es exitoso, aquí podemos redirigir al usuario o mostrar algo
       console.log('Login exitoso:', response.token);
-      // Redirigir o actualizar el estado de la app
     } catch {
       setErrorMessage('No se pudo iniciar sesión. Verifica tus credenciales.');
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#14213D]">
       {/* Mensaje de bienvenida */}
-      <div className="text-center mb-6">
-        <h1 className="text-3xl font-bold text-gray-800">Bienvenido a EbenConta</h1>
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold text-white">Bienvenido a EbenConta</h1>
       </div>
 
       {/* Formulario de inicio de sesión */}
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
+      <div className="bg-[#E5E5E5] p-8 rounded-xl shadow-xl max-w-sm w-full">
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Iniciar sesión</h2>
         
         <form onSubmit={handleSubmit}>
@@ -42,7 +38,7 @@ const LoginPage = () => {
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Introduce tu correo"
               required
-              className="mt-2 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
+              className="mt-2 p-3 w-full border border-gray-400 rounded-lg focus:ring-2 focus:ring-[#FCA311] outline-none text-black"
             />
           </div>
 
@@ -54,7 +50,7 @@ const LoginPage = () => {
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Introduce tu contraseña"
               required
-              className="mt-2 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-500"
+              className="mt-2 p-3 w-full border border-gray-400 rounded-lg focus:ring-2 focus:ring-[#FCA311] outline-none text-black"
             />
           </div>
 
@@ -64,7 +60,7 @@ const LoginPage = () => {
 
           <button
             type="submit"
-            className="w-full bg-indigo-500 text-white py-3 rounded-md hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-500"
+            className="w-full bg-[#FCA311] text-white py-3 rounded-lg hover:bg-[#E68A00] transition-all font-bold"
           >
             Iniciar sesión
           </button>
